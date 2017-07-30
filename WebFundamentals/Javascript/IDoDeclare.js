ec1d604831400b4d241ae8d94b04a4a546def0d1
AnuragDani501 <99anurag99@gmail.com>
2017-07-30 13:47:11 -0700

    IDoDeclare JS hw1

---
+++ WebFundamentals/Javascript/IDoDeclare.js
@@ -0,0 +1,13 @@
+var num1 = -1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var string1 = "John Snow";
+var bool1 = false;
+var bool2 = true;
+var undefined;
+
+console.log("Number 1:", num1);
+console.log("String 1:", string1);
+console.log("Boolean 1:", bool1);
+console.log("Undefined: ", undefined);
