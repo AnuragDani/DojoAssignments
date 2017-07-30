c4114bc4bd1722d256d1ccab83a3f12b914a6a76
AnuragDani501 <99anurag99@gmail.com>
2017-07-30 16:14:35 -0700

    While You Wait JS hw3

---
+++ WebFundamentals/Javascript/Assignment3.js
@@ -0,0 +1,10 @@
+var daysUntilMyBirthday = 4;
+if(daysUntilMyBirthday > 30){
+    console.log(daysUntilMyBirthday, "days until my birthday. Such a long time... :(");
+} else if(daysUntilMyBirthday < 30 && daysUntilMyBirthday >= 5 ){
+    console.log(daysUntilMyBirthday, "DAYS UNTIL MY BIRTHDAY!!!");
+} else if(daysUntilMyBirthday < 5) {
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*\n" +
+        "♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪\n" +
+        "*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
+}
